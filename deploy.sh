db2cc2a9e1c2b3b504fa4f5b81ef7af540a302d7
Ben Nelson <binelson13@gmail.com>
2025-06-27 20:06:57 -0500

    Fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t localhost/pts-ui:latest ./pts-ui --build-arg REACT_APP_API_URL=http://homelab.io/ptsapi
-docker build -t localhost/pts-api:latest -f PTS.Api/Dockerfile .
+docker build -t localhost/pts-api:latest ./PTS.Api
 docker save localhost/pts-ui:latest -o pts-ui.tar
 docker save localhost/pts-api:latest -o pts-api.tar
 k3s ctr images import pts-ui.tar 
